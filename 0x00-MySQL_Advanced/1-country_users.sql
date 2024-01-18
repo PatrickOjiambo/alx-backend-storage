97132f6d4ceaab9e36200b83acf6f15ef226700d
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-01-18 12:33:39 +0300

    fix:  removed delete table

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,5 @@
 --creates a table users following these requirements:
 --check holberton for the requirements
-DROP TABLE IF EXISTS users;
 CREATE TABLE
     IF NOT EXISTS users (
         id INT NOT NULL AUTO_INCREMENT,
