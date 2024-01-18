d93617c3441028971e1013804d63e08a27dda0a5
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-01-18 12:20:59 +0300

    feat: task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,11 @@
+--creates a table users following these requirements:
+--check holberton for the requirements
+CREATE TABLE
+    IF NOT EXISTS users (
+        id INT NOT NULL AUTO_INCREMENT,
+        email VARCHAR(255) NOT NULL,
+        name VARCHAR(255),
+        country ENUM ('US', 'CO', 'TN') NOT NULL,
+        PRIMARY KEY (id),
+        UNIQUE (email)
+    );
