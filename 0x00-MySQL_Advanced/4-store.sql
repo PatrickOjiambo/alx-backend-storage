2ba64b1e3c76bd66b5fe0ce45a93fb35eae7dabe
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-01-18 14:10:56 +0300

    feat: task 4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,10 @@
+-- SQL script that creates a trigger that
+-- decreases the quantity of an item after adding a new order.
+CREATE TRIGGER decrease_item AFTER
+INSERT
+    ON orders FOR EACH ROW
+UPDATE items
+SET
+    quantity = NEW.number - 1
+WHERE
+    name = NEW.item_name
