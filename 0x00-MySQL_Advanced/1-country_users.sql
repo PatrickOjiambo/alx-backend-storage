3af183a26a104761a883e134fd8611349141a133
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-01-18 12:25:26 +0300

    feat: Delete first

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,6 @@
 --creates a table users following these requirements:
 --check holberton for the requirements
+DROP TABLE IF EXISTS users;
 CREATE TABLE
     IF NOT EXISTS users (
         id INT NOT NULL AUTO_INCREMENT,
