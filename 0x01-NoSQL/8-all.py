4a4738e47f5ca52c3f5be1edf0cdbe88cf5a01d8
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-01-23 21:36:27 +0300

    fix: task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,13 @@
 """
 Python function that lists all documents in a collection:
 """
+from typing import List
+from pymongo.collection import Collection
+from typing import List
+from pymongo.collection import Collection
 
 
-def list_all(mongo_collection):
+def list_all(mongo_collection: Collection) -> List[dict]:
     """
     Python function that lists all documents in a collection:
     """
