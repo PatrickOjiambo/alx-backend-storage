180ecb98d89503b7c9f96dc91ad305c1f790023f
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-01-23 21:41:43 +0300

    fix: task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,17 +1,18 @@
-"""
-Python function that lists all documents in a collection:
-"""
 from typing import List
-from pymongo.collection import Collection
-from typing import List
-from pymongo.collection import Collection
+from pymongo.collection import Collection, Cursor
 
 
 def list_all(mongo_collection: Collection) -> List[dict]:
     """
-    Python function that lists all documents in a collection:
+    Lists all documents in a MongoDB collection.
+
+    Parameters:
+    - mongo_collection (Collection): The MongoDB collection to retrieve documents from.
+
+    Returns:
+    - List[dict]: A list containing dictionaries representing the documents in the collection.
     """
     if mongo_collection is not None:
-        documents = mongo_collection.find()
+        documents: Cursor = mongo_collection.find()
         return [post for post in documents]
     return []
